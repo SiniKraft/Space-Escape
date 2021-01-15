22e944ac3600ad71b9c7930558a9aa7d83b8fa58
SiniKraft <natclaudix@gmail.com>
2021-01-15 22:01:14 +0100

    Update

--- scripts/util/FileManager.py
+++ scripts/util/FileManager.py
@@ -1,16 +1,23 @@
 import os
 import shutil
+import importlib
 
-shutil.copyfile(os.path.dirname(__file__) + '/../../ressources/lang/fr_FR.txt',
-                "tmp/lang_fr_FR.py")
+lang_files_to_load = ['fr_FR', 'en_US']
 
-try:
-    from tmp.lang_fr_FR import fr_FR_lang
-except:
-    shutil.copyfile('default/lang/fr_FR.py',
-                    os.path.dirname(__file__) + "/../../ressources/lang/fr_FR.txt")
-    shutil.copyfile(os.path.dirname(__file__) + '/../../ressources/lang/fr_FR.txt',
-                    "tmp/lang_fr_FR.py")
-    print("[ERROR]: Failed to launch lang resource 'fr_FR', using default !")
-    from tmp.lang_fr_FR import fr_FR_lang
+lang_number = len(lang_files_to_load)  # Count the number of files entries
 
+for x in range(0, lang_number):
+    shutil.copyfile(os.path.dirname(__file__) + '/../../ressources/lang/' + lang_files_to_load[x] + ".txt",
+                    "tmp/lang_" + lang_files_to_load[x] + ".py")  # will copy resource/lang file into tmp/lang.
+
+for x in range(0, lang_number):
+    try:
+        importlib.import_module("tmp.lang_" + lang_files_to_load[x])  # load modified lang file
+    except:
+        shutil.copyfile('default/lang/' + lang_files_to_load[x] + '.py',
+                        os.path.dirname(__file__) + "/../../ressources/lang/" + lang_files_to_load[x] + ".txt")
+        shutil.copyfile(os.path.dirname(__file__) + '/../../ressources/lang/' + lang_files_to_load[x] + '.txt',
+                        "tmp/lang_" + lang_files_to_load[x] + ".py")
+        print("[ERROR]: Failed to launch lang resource '" + lang_files_to_load[x] + "', using default !")
+        importlib.import_module("tmp.lang_" + lang_files_to_load[x])
+        # will copy default to modified, because error where found.
---
+++ scripts/util/default/lang/en_US.py
@@ -0,0 +1,18 @@
+#
+# Lang file for fr_FR
+# Please see READ ME file before doing any modifications
+#
+# Please do not edit this :
+
+max_number = 2
+
+en_US_lang = []
+
+for x in range(0, max_number + 1):
+    en_US_lang.append("Not defined")
+
+# Now you can edit ONLY if you've read the READ ME file.
+
+en_US_lang[0] = "Play"
+en_US_lang[1] = "Settings"
+en_US_lang[2] = "Quit"
